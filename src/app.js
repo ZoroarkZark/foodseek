9a76366d1f33b452b2dd1bca96751f89fe365426
CalBlanco <72684566+Jimdangle@users.noreply.github.com>
2022-10-07 14:56:36 -0700

    SQL server should hopefully be set up on my personal computer with portfwding for remote access. Added a method to add a user to the database. However its super buggy and can only do one addition per load of the app. Some handshake issue

--- src/app.js
+++ src/app.js
@@ -5,14 +5,18 @@ const port = 3000;
 
 require('dotenv').config({path: __dirname +'/.env'}); // fix .env path 
 
+// SQL connection structure
+// Gets sql login info from the ./env file
 var db_connection = database.createConnection( {
 	host: process.env.DB_HOST,
 	port: process.env.DB_PORT,
 	user: process.env.DB_USER,
-	password: process.env.DB_USER_PASSWORD,
+	password: process.env.DB_PASS,
 	database: process.env.DB_ACTIVE_DB
 } );
-	
+
+// Basic input 
+// Doesnt do anything but respond with a json
 app.get('/test', (req,res) => {
 	res.statuscode = 200;
 	res.setHeader('Content-Type','application/json');
@@ -24,18 +28,47 @@ app.get('/test', (req,res) => {
 	);
 });
 
-app.put('/pt', (req,res) => {
-	console.log(req.params);
-	res.statuscode = 200;
-	if(req.param("test")){
-		console.log("found param");
+// "Create" a new user by calling a put with the specified parameters 
+app.put('/newuser', (req,res) => { 
+	if(req.param("email") && req.param("pass")){ // check for the required headers 
+		db_connection.connect(function(err) { // database connection using db_connect from above
+			console.log("connection started");
+			if(err) throw err;
+			// SQL code is typed as a variable and then passed to a function
+			var SQL_CODE = 'INSERT INTO user_data (user_email, password) VALUES (' + db_connection.escape(req.param("email"))+", "  + db_connection.escape(req.param("pass"))+')';
+			db_connection.query(SQL_CODE, function(err, results,fields) {
+				if (err) throw err;
+				console.log("Added user!");
+			});
+		});
+		//Response formation
+		console.log("formulating response object");
+		res.statuscode = 200;
+		res.setHeader('Content-Type', 'application/json');
+		res.end(
+			JSON.stringify({
+				new_user: req.param("email"),
+				new_pass: req.param("pass")
+			})
+			);
+		console.log("attempting to end connection");
+		// End the connection (still strugling to get this to work and its generating errors when we don't end a connection)
+		db_connection.end(function (err) {
+			if(err) throw err;
+		});
+		console.log("Connection Ended");
 	}
 	else{
-		console.log("no param found"); 
-	}
-	res.end();
+	res.statuscode = 400;}
+	
 });
 
+// Code to get user information here
+app.get('/getuser', (req,res) => {
+	if(req.param("email") && req.param("pass")){}
+});
+
+// keeps this app open on the specifed port
 app.listen(port, () => {
 	console.log('listening on port ${port}');
 });
