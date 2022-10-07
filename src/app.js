a3b87373492f7456596a0362cd582ecc7229bc9d
CalBlanco <72684566+Jimdangle@users.noreply.github.com>
2022-10-07 15:15:30 -0700

    Fixed the connection issue when trying to submit multiple new users to the database. Currently writting to the database successfully

--- src/app.js
+++ src/app.js
@@ -7,13 +7,16 @@ require('dotenv').config({path: __dirname +'/.env'}); // fix .env path
 
 // SQL connection structure
 // Gets sql login info from the ./env file
-var db_connection = database.createConnection( {
+// Switched to using a pool connection for mulitple uploads
+// wont need the user to have a pool connection but our server can so it can upload to the database with out issue
+var db_pool = database.createPool({
+	connectionLimit: 10,
 	host: process.env.DB_HOST,
 	port: process.env.DB_PORT,
 	user: process.env.DB_USER,
 	password: process.env.DB_PASS,
 	database: process.env.DB_ACTIVE_DB
-} );
+});
 
 // Basic input 
 // Doesnt do anything but respond with a json
@@ -31,15 +34,12 @@ app.get('/test', (req,res) => {
 // "Create" a new user by calling a put with the specified parameters 
 app.put('/newuser', (req,res) => { 
 	if(req.param("email") && req.param("pass")){ // check for the required headers 
-		db_connection.connect(function(err) { // database connection using db_connect from above
-			console.log("connection started");
-			if(err) throw err;
-			// SQL code is typed as a variable and then passed to a function
-			var SQL_CODE = 'INSERT INTO user_data (user_email, password) VALUES (' + db_connection.escape(req.param("email"))+", "  + db_connection.escape(req.param("pass"))+')';
-			db_connection.query(SQL_CODE, function(err, results,fields) {
-				if (err) throw err;
-				console.log("Added user!");
-			});
+		var SQL_CODE = 'INSERT INTO user_data (user_email, password) VALUES (' + db_connection.escape(req.param("email"))+", "  + db_connection.escape(req.param("pass"))+')';
+
+		// The pool code made this part like 1 line which is really nice
+		db_pool.query(SQL_CODE, function (err, results, fields) {
+			if (err) throw err;
+			console.log("added user to db");
 		});
 		//Response formation
 		console.log("formulating response object");
@@ -51,12 +51,7 @@ app.put('/newuser', (req,res) => {
 				new_pass: req.param("pass")
 			})
 			);
-		console.log("attempting to end connection");
-		// End the connection (still strugling to get this to work and its generating errors when we don't end a connection)
-		db_connection.end(function (err) {
-			if(err) throw err;
-		});
-		console.log("Connection Ended");
+		
 	}
 	else{
 	res.statuscode = 400;}
@@ -66,6 +61,17 @@ app.put('/newuser', (req,res) => {
 // Code to get user information here
 app.get('/getuser', (req,res) => {
 	if(req.param("email") && req.param("pass")){}
+});
+
+
+// Close the connection pool started on initialization
+app.get('/closecons', (req,res) => {
+	res.statuscode = 200;
+	res.end();
+	console.log("Closing Pool");
+	db_pool.end(function (err) {
+		if (err) throw err;
+	});
 });
 
 // keeps this app open on the specifed port
