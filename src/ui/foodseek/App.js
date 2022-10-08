6e31631a418e503a3ec5dc712c3761befe12524c
ZoroarkZark <ilikepie1464@gmail.com>
2022-10-08 15:00:46 -0700

    Added React navigation

--- src/ui/foodseek/App.js
+++ src/ui/foodseek/App.js
@@ -1,57 +1,101 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 import { ThemeProvider } from "react-native-rapi-ui";
 import { Layout, TopNav } from 'react-native-rapi-ui'
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { useTheme, themeColor } from 'react-native-rapi-ui';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator()
+
 
 const App = () => {
   return (
-    <ThemeProvider theme="light">
-      <Layout>
-        <TopNav middleContent="Home Screen" />
-        <TopNav
-          leftContent={
-            <Ionicons name="chevron-back" size={20} color={themeColor.black} />
-          }
-          leftAction={() => console.log('back icon pressed')}
-          middleContent="Profile"
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Home Screen' }}
         />
-        <TopNav
-          rightContent={
-            <Ionicons name="ellipsis-vertical" size={20} color={themeColor.black} />
-          }
-          rightAction={() => console.log('setting icon pressed')}
-          middleContent="Settings"
+        <Stack.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{ title: 'Profile Screen'}}
         />
-        <TopNav middleContent={
-          <TextInput
-            style={textStyles.input}
-            placeholder="Let's gooooo!"
-            keyboardType="default"
-            multiline={true}
-            numberOfLines={3}
-            />
-          }
-        /> 
-        <TopNav middleContent={
-          <TextInput
-            style={textStyles.input}
-            placeholder="Let's gooooo!"
-            keyboardType="default"
-          />
-        }
-      /> 
-      </Layout>
-      <View style={styles.container}>
-        <Text>Hello FoodSeek!</Text>
-        <StatusBar style="auto" />
-      </View>
-    </ThemeProvider>
+    </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
+const HomeScreen = ({ navigation }) => {
+  return (
+  <ThemeProvider theme="light">
+          <Layout>
+            <TopNav
+              middleContent={
+                <Button
+                  title="Profile"
+                  onPress={() =>
+                  navigation.navigate('Profile', { name: 'Ferdy' })
+                   }
+                />
+              }
+            />
+            <TopNav
+              rightContent={
+                <Ionicons name="ellipsis-vertical" size={20} color={themeColor.black} />
+              }
+              rightAction={() => console.log('setting icon pressed')}
+              middleContent="Settings"
+            />
+            <TopNav middleContent={
+              <TextInput
+                style={textStyles.input}
+                placeholder="Let's gooooo!"
+                keyboardType="default"
+                multiline={true}
+                numberOfLines={3}
+                />
+              }
+            /> 
+            <TopNav middleContent={
+              <TextInput
+                style={textStyles.input}
+                placeholder="Let's gooooo!"
+                keyboardType="default"
+              />
+            }
+          /> 
+          </Layout>
+          <View style={styles.container}>
+            <Text>Hello FoodSeek!</Text>
+            <StatusBar style="auto" />
+          </View>
+        </ThemeProvider>
+  );
+};
+
+const ProfileScreen = ({ navigation, route }) => {
+  return (
+  <ThemeProvider theme="light">
+  <Layout>
+    <TopNav
+      middleContent={
+       <Button
+        title="Home"
+        onPress={() =>
+        navigation.navigate('Home')
+         }
+      />
+    }
+  />
+   </Layout>
+   </ThemeProvider>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
