4c342ddaf1d289f3fc349db858e2e6e0fea83f22
CalBlanco <72684566+Jimdangle@users.noreply.github.com>
2022-10-08 14:19:58 -0700

    1) added a error handler method for non-fatal errors. 2) cleaned up the new user function a little bit.

--- src/server/app.js
+++ src/server/app.js
@@ -10,8 +10,6 @@ const hostname = "localhost";
 // Express app 
 const app = express();
 
-
-
 require('dotenv').config({path: __dirname +'/.env'}); // fix .env path 
 
 // SQL connection structure
@@ -27,6 +25,49 @@ var db_pool = database.createPool({
 	database: process.env.DB_ACTIVE_DB
 });
 
+
+
+
+// util function for error responses
+// takes in the error object and checks if we have a method of handling
+// takes in a response object to return if we do have a handler method, if not throws an exception and closes
+// returns a err_response list [0:statuscode, 1:content, 2:fatal] fatal is 1 for fatal, 0 for non fatal
+// remove this shit in the future its trash
+function errRespond(err){
+	let out = []
+	switch(err.errno){
+		case 1062: // Duplicate entry. The SQL server is set to only allow unique emails so it will throw an error for duplicates
+			console.log("Duplicate Entry, Non-Fatal for us");
+			out[0] = 400;
+			out[1] = JSON.stringify({
+				msg: "User Already has an account! try signing in."
+				});
+			out[2] = 0; // non fatal
+			return out;
+			break;
+		default: // Add other errnos above 
+			out[0] = 500;
+			out[1] = JSON.stringify({
+				msg: "FATAL ERROR!"
+				});
+			out[2] = 1;
+			return out;
+	}
+}
+
+
+// call when terminating to ensure db pool is closed 
+// can also just be used to call things on termination in general hopefully
+function terminate(err){
+	
+	// Pools closed bitch
+	db_pool.end(function (err) {
+		if (err) throw err;
+	});
+	
+	throw err; // throw the error to console and halt program
+
+}
 // Basic input 
 // Doesnt do anything but respond with a json
 app.get('/test', (req,res) => {
@@ -40,36 +81,35 @@ app.get('/test', (req,res) => {
 	);
 });
 
+
+// create an error handler response 
+// pass in the given error from the function and then generate response
+
 // "Create" a new user by calling a put with the specified parameters 
 app.post('/newuser', (req,res) => { 
-	console.log(req.query);
+	res.setHeader('Content-Type', 'application/json'); // set response to be a json 
+		
 	if(req.query.email && req.query.pass){ // check for the required headers 
 		var insert_user = 'INSERT INTO user_data (user_email, password) VALUES (' + db_pool.escape(req.query.email)+", "  + db_pool.escape(req.query.pass)+')';
 		console.log(`Attempting to insert ${req.query.email} ${req.query.pass}`)
 		// The pool code made this part like 1 line which is really nice
 		db_pool.query(insert_user, function (err) {
 			if (err){
-				console.log(`Pass : ${process.env.DB_PASS}`);
-				throw err;
+				let err_response = errRespond(err);	// get error response info
+				res.statuscode = err_response[0]; // place into the response
+				res.end(err_response[1]);
+				
+				if(err_response[2] == 1){
+					terminate(err);
+				}
 			}
-			console.log("added user to db");
+			else{
+				console.log("User added successfully");
+			}
 		});
-		//Response formation
-		
-		res.statuscode = 200;
-		res.setHeader('Content-Type', 'application/json');
-		res.end(
-			JSON.stringify({
-				new_user: req.query.email,
-				new_pass: req.query.pass
-			})
-			);
-		
 	}
 	else{
-		console.log("Couldn't find parameters");
-		res.statuscode = 400;
-	}
+		
 	
 });
 
