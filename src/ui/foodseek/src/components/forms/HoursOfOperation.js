2406be9fb0f5533bca1f0dda151cbf5c59547bd7
ZoroarkZark <ilikepie1464@gmail.com>
2022-10-24 21:50:38 -0700

    Changed, making new file

--- src/ui/foodseek/src/components/forms/HoursOfOperation.js
+++ src/ui/foodseek/src/components/forms/HoursOfOperation.js
@@ -79,7 +79,7 @@ const week = {
 
 
 
-const TimeRange = ({type="label", labelStart="Start", labelEnd="End", value, onChange}) => {
+export const TimeRange = ({type="label", labelStart="Start", labelEnd="End", value, onChange}) => {
     if (type === "label") {
         return (
             <>
@@ -116,38 +116,4 @@ const TimeRange = ({type="label", labelStart="Start", labelEnd="End", value, onC
 
 
 
-export const HoursOfOperation = ({type="to", start: labelStart="Start", end: labelEnd="End", value, onChange}) => {
-    return (
-        <View>
-                <Text>Monday (leave blank if closed)</Text>
-                <View style={{flexDirection: 'row', justifyContent: 'center'}}>
-                    <TimeRange type={type} value={{start: value.monday.start, end: value.monday.end}} onChange={{start: onChange.monday.start, end: onChange.monday.end}} />
-                </View>
-                <Text>Tuesday (leave blank if closed)</Text>
-                <View style={{flexDirection: 'row', justifyContent: 'center'}}>
-                    <TimeRange type={type} value={{start: value.tuesday.start, end: value.tuesday.end}} onChange={{start: onChange.tuesday.start, end: onChange.tuesday.end}} />
-                </View>
-                <Text>Wednesday (leave blank if closed)</Text>
-                <View style={{flexDirection: 'row', justifyContent: 'center'}}>
-                    <TimeRange type={type} value={{start: value.wednesday.start, end: value.wednesday.end}} onChange={{start: onChange.wednesday.start, end: onChange.wednesday.end}} />
-                </View>
-                <Text>Thursday (leave blank if closed)</Text>
-                <View style={{flexDirection: 'row', justifyContent: 'center'}}>
-                    <TimeRange type={type} value={{start: value.thursday.start, end: value.thursday.end}} onChange={{start: onChange.thursday.start, end: onChange.thursday.end}} />
-                </View>
-                <Text>Friday (leave blank if closed)</Text>
-                <View style={{flexDirection: 'row', justifyContent: 'center'}}>
-                    <TimeRange type={type} value={{start: value.friday.start, end: value.friday.end}} onChange={{start: onChange.friday.start, end: onChange.friday.end}} />
-                </View>
-                <Text>Saturday (leave blank if closed)</Text>
-                <View style={{flexDirection: 'row', justifyContent: 'center'}}>
-                    <TimeRange type={type} value={{start: value.saturday.start, end: value.saturday.end}} onChange={{start: onChange.saturday.start, end: onChange.saturday.end}} />
-                </View>
-                <Text>Sunday (leave blank if closed)</Text>
-                <View style={{flexDirection: 'row', justifyContent: 'center'}}>
-                    <TimeRange type={type} value={{start: value.sunday.start, end: value.sunday.end}} onChange={{start: onChange.sunday.start, end: onChange.sunday.end}} />
-                </View>
-        </View>
 
-    );
-}
--- src/ui/foodseek/src/screens/hours.js
+++ src/ui/foodseek/src/screens/hours.js
@@ -1,11 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 import { StyleSheet, Text, View, Button } from 'react-native';
-import { HoursOfOperation } from '../components/forms';
-import { TimeInput } from '../components/common';
+import { TimeInput } from "../components/common";
+import { TimeRange } from "../components/forms";
 
 export const HoursOfOperationScreen = ({navigation}) => {
+    const [mondayTime, setMondayTime] = useState(new Date());
+    const [tuesdayTime, settuesdayTime] = useState(new Date());
+    const [weddayTime, setweddayTime] = useState(new Date());
+    const [thursdayTime, setthursdayTime] = useState(new Date());
+    const [fridayTime, setfridayTime] = useState(new Date());
+    const [saturdayTime, setsaturdayTime] = useState(new Date());
+    const [sundayTime, setsundayTime] = useState(new Date());
+    const [show, setShow] = useState(true);
+
+    const onChange = (event, selectedDate) => {
+        const currentDate = selectedDate;
+        setMondayTime(currentDate);
+    }
+
     return (
-        <HoursOfOperation value={''} onChange={''}/>
+        <View>
+            <Text>Monday</Text>
+            <TimeInput value={mondayTime} onChange ={onChange}/>
+            <Text>{mondayTime.toLocaleTimeString('en-US', {timeStyle: 'short'})}</Text>
+            
+            {/*<Text>Tuesday</Text>
+            <TimeInput value={new Date()} onChange ={new Date()}/>
+            <Text>Wednesday</Text>
+            <TimeInput value={new Date()} onChange ={new Date()}/>
+            <Text>Thursday</Text>
+            <TimeInput value={new Date()} onChange ={new Date()}/>
+            <Text>Friday</Text>
+            <TimeInput value={new Date()} onChange ={new Date()}/>
+            <Text>Saturday</Text>
+            <TimeInput value={new Date()} onChange ={new Date()}/>
+            <Text>Sunday</Text>
+    <TimeInput value={new Date()} onChange ={new Date()}/>*/}
+
+        </View>
     );
 };
 
