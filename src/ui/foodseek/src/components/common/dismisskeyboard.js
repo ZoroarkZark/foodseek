508ee8e14bbd10115bc7ce3d31302eaeaefcadc7
Nisse Spicer <52335555+nnisse@users.noreply.github.com>
2022-11-04 14:03:13 -0700

    ScrollViewDismissKeyboard wrapper works, the View wrapper is not recommended... updated files previously using broken DismissKeyboard

--- src/ui/foodseek/src/components/common/dismisskeyboard.js
+++ src/ui/foodseek/src/components/common/dismisskeyboard.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Keyboard } from 'react-native'
 import { ScrollView, TouchableWithoutFeedback } from 'react-native-gesture-handler'
 
-// Wrap around outermost View component of the application to enable dismissal of keyboard whenever the screen is pressed
+// NOT RECOMMENDED: Wrap around outermost View component of the application to enable dismissal of keyboard whenever the screen is pressed
 export const ViewDismissKeyboard = ( { children } ) => {
     return (
         <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false} >
@@ -11,7 +11,7 @@ export const ViewDismissKeyboard = ( { children } ) => {
     )
 }
 
-// Wrapper for outermost scroll view component of the application to enable dismissal of keyboard whenever the screen apart from buttons and text is pressed
+// RECOMMENDED: Wrapper for outermost scroll view component of the application to enable dismissal of keyboard whenever the screen apart from buttons and text is pressed
 export const ScrollViewDismissKeyboard = ( { children } ) => { 
     return (
         <ScrollView keyboardShouldPersistTaps='handled'>
