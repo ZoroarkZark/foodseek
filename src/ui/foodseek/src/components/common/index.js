8ae66498e4d2181793306f5be48159ad1426bd47
ZoroarkZark <ilikepie1464@gmail.com>
2022-11-09 11:36:25 -0800

    Search page created

--- src/ui/foodseek/src/components/common/index.js
+++ src/ui/foodseek/src/components/common/index.js
@@ -7,6 +7,7 @@ import CustomTextInput from './textinput'
 import PickerInput from './pickerinput'
 import { TransitPickerInput } from './pickerinput'
 import TimeInput from './timeinput'
+import SearchInput from './searchinput'
 import FavoriteButton from './favoritebutton'
 import Avatar from './avatar'
 import ViewMap from './map'
@@ -16,6 +17,7 @@ export {
     TextButton,
     PasswordInput,
     EmailInput,
+    SearchInput,
     ViewDismissKeyboard,
     ScrollViewDismissKeyboard,
     CustomTextInput,
