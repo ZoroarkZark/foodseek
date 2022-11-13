840a0569303c84cefe0329ae6715657b27fc8e63
nairoldi <nairoldi@ucsc.edu>
2022-11-13 11:27:14 -0800

    made changes to updatepass

--- src/server/routes/core.js
+++ src/server/routes/core.js
@@ -220,104 +220,34 @@ CoreRouter.post('/validatecode', (req, res) => {
 });
 
 // post data here to set a update new password with link
-CoreRouter.get('/updatepass', (req, res,next)=> {
+CoreRouter.post('/updatepass', (req, res,next)=> {
     //next();
     const resbody = new sutil.res_obj();
-    sutil.verify(req.query.token, (err, type) => { // jwt check
-        if(err){
-            resbody.setIssue(2); // bad jwt
-            res.end(resbody.package());
-            return;
-        }
-    });
-    //  code to set pass as token and email them their password
-    let token = randtoken.generate(20);
-    let decoded = jwt_decode(req.query.token);
-    let email = decoded.user;
-    let subject = 'Reset Password Token';
-    let html_str = '<p>You requested for reset password, kindly use this ' + token + ' to sign in </p>';
-    let mailOptions = createOptions(email, subject, html_str)
-    let sent = sendEmail(mailOptions);
-    if(sent == 1){
-        resbody.setIssues(10)
-        res.end(resbody.package());
-        return;
-    }
-    
-    bcrypt.hash(token, 10, (err, hash)=>{
-        if(err){
-            resbody.setIssue(4);
-            res.end(resbody.package());
-            return;
-        }
-        
-        if(hash) {
-            Store.setPassword(email, hash, (err) => {
-                if(err){
-                resbody.setIssue(7);
-                res.end(resbody.package());
-                }
-            });
-        }
-        else {
-            resbody.setIssue(5);
-            res.end(resbody.package());
-            return;
-        }
-    });
-});
-
-// post data here to set a new password
-CoreRouter.post('/newpass', (req, res,next)=> {
-    //next();
-    const resbody = new sutil.res_obj();
-    if(sutil.validate(['email','old_pass','new_pass'], req.body)){
-        Store.getUser(req.body.email, (err, result) => { // get the user 
+    if(sutil.validate(['email','pass'], req.body)){       
+        bcrypt.hash(req.body.pass, 10, (err, hash)=>{
             if(err){
-                console.error(err);
-                resbody.setIssues(err);
+                resbody.setIssue(4);
                 res.end(resbody.package());
                 return;
             }
-            //console.log(result);
-            if(result){
-                bcrypt.compare(req.body.old_pass, result.password, (error, hash) => {
-                    if(error){
-                        resbody.setIssue(4,"problem de-hashing pass");
-                        res.end(resbody.package());
-                        return;
-                    }
-                        
-                    if(hash){
-                        bcrypt.hash(req.body.new_pass, 10, (err, hash)=>{
-                            if(err){
-                                resbody.setIssue(4);
-                                res.end(resbody.package());
-                                return;
-                            }        
-                           
-                            Store.setPassword(req.body.email, hash, (err, results) => {
-                                if(err){
-                                    resbody.setIssue(7);
-                                    res.end(resbody.package());
-                                    return;
-                                }
-                                resbody.setData({msg: "password changed"});
-                                res.end(resbody.package());
-                                return;
-                            });
-                        });
+        
+            if(hash) {
+                Store.setPassword(req.body.email, hash, (err) => {
+                    if(err){
+                    resbody.setIssue(7);
+                    res.end(resbody.package());
                     }
                 });
             }
-        }); 
-    }
-    else {
-        resbody.setIssues(1)
-        res.end(resbody.package());
-        return;
+            else {
+                resbody.setIssue(5);
+                res.end(resbody.package());
+                return;
+            }
+        });
     }
 });
+
 
 
 CoreRouter.get('/confirmEmail', (req,res) => {
