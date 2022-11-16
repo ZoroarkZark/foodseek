1c52ace1e6f6b9d3f1ef21f148dfce63ae22d11f
Nisse Spicer <52335555+nnisse@users.noreply.github.com>
2022-11-16 03:14:33 -0800

    created the bottom sheet component and added the search to it

--- src/ui/foodseek/src/features/seeker/screens/view-post-map-feature/map-view.js
+++ src/ui/foodseek/src/features/seeker/screens/view-post-map-feature/map-view.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, useContext } from 'react'
-import { Animated, Easing, Text, View } from 'react-native'
+import React, { useState, useEffect, useContext, useRef, useMemo, useCallback } from 'react'
+import { Text, View } from 'react-native'
 import ViewMap from '../../../../components/common/map.js'
 import { map as style } from '../../../../style/styleSheet.js'
 import { Ionicons } from '@expo/vector-icons'
@@ -7,24 +7,103 @@ import TextButton from '../../../../components/common/textbutton.js'
 import { LocationContext } from '../../../../context/LocationContext.js'
 import { Marker } from 'react-native-maps'
 import { AuthenticationContext } from '../../../../context/AuthenticationContext.js'
-import { useSharedValue } from 'react-native-reanimated'
+import BottomSheet, { BottomSheetSectionList, BottomSheetTextInput } from '@gorhom/bottom-sheet'
+import { AutocompleteSearchBar } from '../../../../components/api/AutocompleteSearchBar.js'
+import { FoodCardContext } from '../../../../context/FoodCardContext.js'
 
 // stretchy container will contain the search bar
 
+const BottomSheetContainer = ( { setCards, onLocate } ) => {
+    const { location: loc, keyword: key, } = useContext( LocationContext ) // load the initial variables for the device
+    const { onRefresh } = useContext( FoodCardContext )
+    const [ keyword, setKeyword ] = useState( key )
+    const [ location, setLocation ] = useState( loc )
 
-const StretchyContainer = () => {
-    const translateX = setVal( 0 )
-    const isSliding = useSharedValue( false )
+    const sheetRef = useRef < BottomSheet > ( null )
+
+    const search = useCallback( () => { 
+        onRefresh( location, setCards )
+    },
+        [] )
     
-    const size = 
+    const sections = useMemo( () =>
+        Array( 10 )
+            .fill(0)
+            .map((_, index) => ({
+            title: `Section ${index}`,
+            data: Array(10)
+                .fill(0)
+                .map((_, index) => `Item ${index}`),
+    })),
+        [] )
+    const snapPoints = useMemo( () => [ '25%', '50%', '90%' ], [] ) // snap points for the bottom sheet display coverage
     
+    const handleSheetChange = useCallback( ( index ) => {
+        console.log( 'handleSheetChange', index )
+    }, [] )
+    
+    const handleSnapPress = useCallback( ( index ) => {
+        sheetRef.current?.snapToIndex(index)
+    }, [] )
+    
+    const handleClosePress = useCallback( (  ) => {
+        sheetRef.current?.close()
+    },
+        [] )
+    
+
+    // used to render section header
+    const renderSectionHeader = useCallback( ( { item } ) => (
+        <BottomSheetTextInput>
+            <View style={{ paddingTop: 110, padding: 10, paddingBottom: 10}} >
+                <AutocompleteSearchBar
+                    {...
+                        {
+                            setKeyword,
+                            setLocation,
+                            search,
+                        }
+                    }
+                />
+            </View>
+         </BottomSheetTextInput>
+    ), [] ) // end renderSectionHeader
+
+    // used to render section items
+    const renderItem = useCallback( ( { item } ) => (
+        <></>
+    ), [] ) // end renderItem
+    
+
+    useEffect( () => {
+        if ( !location ) return
+        onLocate( location )
+    }, [ location, setLocation ] )
+    
+    return (
+        <View style={{}}>
+            <BottomSheet
+                ref={sheetRef}
+                index={1}
+                snapPoints={snapPoints}
+                onChange={handleSheetChange}
+                keyboardBehavior='fillParent'
+            >
+                <BottomSheetSectionList
+                    sections={sections}
+                    keyExtractor={( i ) => i}
+                    renderSectionHeader={renderSectionHeader}
+                    renderItem={renderItem}
+                    contentContainerStyle={{}}
+                />
+            </BottomSheet>
+            
+        </View>
+    )
 
 }
 
-const searchStyle = {elevatedElement: {
-    zIndex: 3, // works on ios
-    elevation: 3, // works on android
-  }}
+
 export const Map = ( { navigation, route } ) => {
     const {user} = useContext(AuthenticationContext)
     const { deviceLocation, getLocation, setLocation } = useContext(LocationContext)
