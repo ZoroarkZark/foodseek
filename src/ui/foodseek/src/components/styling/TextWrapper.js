752fb753b62829de50050e949142aea0b91c5a31
Mofokkin Adam <aelaidy@ucsc.edu>
2022-11-19 15:11:31 -0800

    tags

--- src/ui/foodseek/src/components/styling/TextWrapper.js
+++ src/ui/foodseek/src/components/styling/TextWrapper.js
@@ -2,12 +2,14 @@ import React, { useContext } from 'react'
 import { Text } from 'react-native'
 import { ThemeContextProvider, ThemeContext } from '../../context/ThemeContext'
 
-const TextWrapper = ({ children, fontSize, textAlign }) => {
+const TextWrapper = ({ children, fontSize, textAlign, paddingLeft, paddingRight }) => {
     const {darkTheme, toggleTheme} = useContext(ThemeContext);
 
     const style = {
             textAlign: textAlign ? textAlign : 'left',
             padding: 10,
+            paddingLeft: paddingLeft ? paddingLeft : 10,
+            paddingRight: paddingRight ? paddingRight : 10,
             fontSize: fontSize ? fontSize : 25,
             color: darkTheme ? '#CCC' : '#333',
     }
--- src/ui/foodseek/src/features/vendor/screens/create-post-feature/create-post.js
+++ src/ui/foodseek/src/features/vendor/screens/create-post-feature/create-post.js
@@ -8,47 +8,76 @@ import TextWrapper from '../../../../components/styling/TextWrapper';
 
 export const CreatePost = ( { route, navigation } ) => {
     const [foodName, setFoodName] = useState('');
+    const [tags, setTags] = useState();
+    const [testTags, setTestTags] = useState('');
+    const [currentTags, setCurrentTags] = useState('');
+    const [numTags, setNumTags] = useState(0);
     const [timeAvailable, setTimeAvailable] = useState('');
     const uri = (route.params) ? route.params.uri : "";
     console.log(`recieved uri ${uri}`);
 
-    function checkInputs(){
+    //To do, make this not navigate after picture is taken
+    function checkInputs(input){
         const nameRegex = /^[A-Z]+$/i;
         console.log(nameRegex.test(foodName));
         if (nameRegex.test(foodName)) {
             navigation.navigate('Camera');
         } else {
-            //give the error
-            //Alert.alert()
+            alert('Improper input')
         }
+    }
+
+    function checkTags(){
+        const ar = currentTags.split('#')
+        let t = ''
+        for (let i = 1; i < ar.length; i=i+1){
+            t = t + ' #' + ar[i]
+        }
+        setTestTags(t)
     }
 
     if (uri == ""){
         return (
             <ScrollViewDismissKeyboard>
                 <Container>
-                <Title>Upload Food</Title>
-                <Button text='Take Picture' style={{marginTop:20, marginBottom:20}} onPress={() => checkInputs()}/>
-                    <TextInput style={styles.input} value={foodName} placeholder="Enter Food Name..." onChangeText={setFoodName}/>
-                    <TextWrapper>{foodName}</TextWrapper>
+                    <Title>Upload Food</Title>
+                    <Button text='Take Picture' style={{marginTop:20, marginBottom:20}} onPress={() => checkInputs(foodName)}/>
+                    <TextInput style={styles.input} value={foodName} placeholder="Enter Item Name..." onChangeText={setFoodName}/>
                 </Container>
             </ScrollViewDismissKeyboard>
         );
     } else {
         return (
-            <Container>
-                <TextWrapper>Food Name: {foodName}</TextWrapper>
-                <Image alt='image didnt load' style={styles.image} source = {{uri: uri}}/>
-            </Container>
+            <ScrollViewDismissKeyboard>
+                <View style={styles.container}>
+                    <TextWrapper>New Item</TextWrapper>
+                    <View style={styles.row}>
+                        <Image alt='image didnt load' style={styles.image} source = {{uri: uri}}/>
+                        <Text style={styles.text2}>Item Name: {foodName}</Text>
+                    </View>
+                    <Text>{testTags}</Text>
+                    <View style={styles.row}>
+                        <TextInput style={styles.input} value={currentTags} placeholder="Enter Tags..." onChangeText={setCurrentTags}/>
+                        <Button text='Testing button' style={{marginTop:20, marginBottom:20}} onPress={() => checkTags()}/>
+                    </View>
+                    <Button text='Submit Post' style={{marginTop:20, marginBottom:20}} onPress={() => alert('not implemented yet')}/>
+                </View>
+            </ScrollViewDismissKeyboard>
         );
     }
 }
 
 const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
     image: {
-        flex: 1,
-        height: 400,
-        width: 400,
+        resizeMode: 'contain',
+        height: 104,
+        width: 70,
+        borderRadius: 15,
+        borderWidth: 2,
     },
     input: {
       height: 40,
@@ -65,5 +94,16 @@ const styles = StyleSheet.create({
         backgroundColor: 'blue',
         color: 'white',
     },
+    row: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'left',
+        padding: 10,
+    },
+    text2: {
+        fontSize: 25,
+        paddingLeft: 30,
+        paddingRight: 60,
+    }
   });
   
