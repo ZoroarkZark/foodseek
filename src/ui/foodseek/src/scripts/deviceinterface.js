fef0997586704999ba7f548be349e275bc221a51
Nisse Spicer <52335555+nnisse@users.noreply.github.com>
2022-11-20 19:30:13 -0800

    before I screw it all up

--- src/ui/foodseek/src/scripts/deviceinterface.js
+++ src/ui/foodseek/src/scripts/deviceinterface.js
@@ -40,3 +40,42 @@ function buildURL(end,path){
     return `${end}${path}`;
 }
 
+export async function imgFetch(path, jwt, id, location, uri, timestamp, details){
+    const blob = getBlob( uri )
+    url = buildURL(endpoint,url)
+    let response = await fetch('http://127.0.0.1:3000/vendor/upl2', {
+        method: "POST",
+      headers: {
+          Accept: "application/json",
+          "Content-Type": "multipart/form-data",
+        },
+        body: blob
+   });
+    let result = await response.json();
+    console.log(result)
+   return result
+}
+  
+
+export async function fetchRequestIMG(url, method, payload){
+    // get everything ready for the actual fetch call
+    url = buildURL(endpoint,url);
+    method = method.toUpperCase();
+    payload = (method=="GET") ? null : JSON.stringify(payload); //If we have a GET send no body, if we have anything else stringify the body
+    console.log(`${method} @ ${url} : payload ${JSON.stringify(payload)}`);
+
+    // actual fetch call
+    const resp = await fetch(url, {
+        method: method,
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "multipart/form-data",
+        },
+        body: payload
+    })
+    if ( !resp.ok ) { throw new Error(`HTTP error, status = ${resp.status}`) }  // throws an error if fetch call senses an error
+    // json representation of fetch response
+
+    if(!resp.ok){ throw new Error("bad network request");}
+    return resp.json();
+}
