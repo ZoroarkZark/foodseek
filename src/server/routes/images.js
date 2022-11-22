31d40b088a4a3073e5111226dcc7368fe303cf8e
CalBlanco <72684566+Jimdangle@users.noreply.github.com>
2022-11-21 20:55:08 -0800

    deleted things

--- src/server/routes/images.js
+++ src/server/routes/images.js
@@ -80,11 +80,66 @@ ImageRouter.post('/imgtest', async (req,res,next) => {
         chunks.push(buff);
     });
 
-    req.on('end', async () => {
-        let data = Buffer.concat(chunks);
-        let dat_str = ''+data
-        console.log(dat_str);
-    })
+    req.on('end', async ()=> {
+        //console.log(chunks);
+        let data = Buffer.concat(chunks); // this is our base64 image string
+
+        let data_str = ''+data; // actual string
+
+        let mime = getMime(data_str.split(',')[0]); // get the mime / extension type
+
+        let fileName = `${sutils.genToken(20)}${getExt(mime)}`; // create a file name
+
+        // make da image locally to convert from base64 to binary
+        fs.writeFile(path.resolve(__dirname, fileName), data_str.split(',')[1], {encoding:'base64'}, (err) => {
+            if(err) return next(err); // Image write error
+
+            // we wrote the local file so now we can send it to amazon
+            // get the binary file contents
+            fs.readFile(path.resolve(__dirname, fileName), async (err,data) => {
+                if (err) return next(err); // Image read error
+
+                let com = new s3.PutObjectCommand({
+                    Bucket: bucketName,
+                    Key: fileName,
+                    Body: data,
+                    ContentType: in_data.mime,
+                })
+        
+                // send the command
+                await S3.send(com)
+                .then( (data) => {console.log(data);})
+                .catch( (err) => {return next(err); }) 
+
+                // get a link 
+                let link = await getLiveURL(fileName);
+
+                let food_card = {
+                    item: in_data.item,
+                    loc: in_data.loc,
+                    tags: in_data.tags,
+                    timestamp: in_data.timestamp,
+                    img_url: link,
+                    vendor: in_data.vendor
+                }
+
+                FoodStore.uploadMore(food_card, (err) => {
+                    if(err){
+                        return next(7); // SQL error 
+                    }
+                    
+                    resbody.setData({msg:"uploaded image successfully", link:link});
+                    removeFile(fileName); // remove the local file from storage
+
+                    return next();
+
+                });
+            })
+
+        }) 
+
+
+    });
     // Recieve a base64 url of the image
     // the base64 will contain the mime type and img data
     // convert the base64 to the actual image using the mimetype 
