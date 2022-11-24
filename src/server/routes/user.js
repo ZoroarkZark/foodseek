2b5bb36e3e5b07bd36512a989b74c1a0016f54c8
CalBlanco <72684566+Jimdangle@users.noreply.github.com>
2022-11-24 11:03:51 -0800

    back

--- src/server/routes/user.js
+++ src/server/routes/user.js
@@ -40,7 +40,7 @@ UserRouter.post('/list', (req, res, next)=>{
         }
         
         //console.log(results);
-        resbody.setData({msg: "Got List!", cards: results});
+        resbody.setData({msg: "Got List!", items: results});
         return next();
     });
     
